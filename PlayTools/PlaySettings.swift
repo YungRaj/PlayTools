c794ada439e62b4522ca1e4ea0b356cc03dbf4db
Ilhan Raja <ilhan.raja@icloud.com>
2022-09-08 14:47:21 -0700

    Change to iPhone orientation

--- PlayTools/PlaySettings.swift
+++ PlayTools/PlaySettings.swift
@@ -133,11 +133,13 @@ extension Dictionary {
                 return "J320xAP"
             case "iPad13,8":
                 return "J522AP"
+            case "iPhone14,2":
+                return "D63AP"
             default:
-                return "J320xAP"
+                return "D63AP"
             }
         }
-        return "J320xAP"
+        return "D63AP"
     }()
 
     static var isGame: Bool {
